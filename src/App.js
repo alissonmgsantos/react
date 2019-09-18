7e86482c82ca1bb1c4c56ff514bc721fd8a06fb0
Alisson Matos <alissonmgsantos@gmail.com>
2019-09-18 11:21:39 -0300

    Inclusão do css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./css/pure-min.css";
+import "./css/side-menu.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="layout">
+      {/* <!-- Menu toggle --> */}
+      <a href="#menu" id="menuLink" className="menu-link">
+        {/* <!-- Hamburger icon --> */}
+        <span></span>
+      </a>
+
+      <div id="menu">
+        <div className="pure-menu">
+          <a className="pure-menu-heading" href="#a">
+            Company
+          </a>
+
+          <ul className="pure-menu-list">
+            <li className="pure-menu-item">
+              <a href="#a" className="pure-menu-link">
+                Home
+              </a>
+            </li>
+            <li className="pure-menu-item">
+              <a href="#a" className="pure-menu-link">
+                About
+              </a>
+            </li>
+
+            <li className="pure-menu-item menu-item-divided pure-menu-selected">
+              <a href="#a" className="pure-menu-link">
+                Services
+              </a>
+            </li>
+
+            <li className="pure-menu-item">
+              <a href="#a" className="pure-menu-link">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div id="main">
+        <div className="header">
+          <h1>Autor</h1>
+        </div>
+
+        <div className="content">
+      
+        </div>
+      </div>
     </div>
   );
 }
